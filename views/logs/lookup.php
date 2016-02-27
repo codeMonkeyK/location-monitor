80f4e5d0a2ee3bb9a6ee93e68b8e024d82912888
codeMonkeyK <ehminges@ncsu.edu>
2016-02-27 15:50:49 -0500

    added pie chart

--- views/logs/lookup.php
+++ views/logs/lookup.php
@@ -40,43 +40,68 @@
 			$(event.target).css("cursor", "default")
 		});
 
-	})');
+	})'
+	);
 ?>
 
 <div class="row">
-	<h3><u>Alphabetical Listing of Logged IP addresses</u></h3>
 
-	<ol>
-		<?php $i = 0 ?>
-		<?php foreach ($locs as $loc): ?>
-		    <li><span class="location-lbl glyphicon glyphicon-chevron-down" aria-hidden="true"> </span><span class="location-lbl glyphicon glyphicon-chevron-up" aria-hidden="true"> </span><label> &nbsp IP location</label>: <?= Html::encode($loc['loc']) ?> - (count <?= Html::encode($loc['cnt']) ?>)</li>
-	   		<ul class='bottom'>
-			   	<li><label>Status codes</label>:
-				   	<?php $j = 0 ?>
-				   	<?php foreach ($loc['status'] as $statusCode): 
-				   		if ($j < sizeof($loc['status']) - 1) {
-				   	?>		<?= Html::encode($statusCode) ?>,
-					<?php } else {?>
-				   			<?= Html::encode($statusCode) ?>
-				   	<?php } 
-				   		$j++;
-				   	?>
-				   	<?php endforeach; ?>
-			   	</li>
-			   	<li><label>IP Addresses</label>:
-				   	<?php $j = 0 ?>
-				   	<?php foreach ($loc['ips'] as $ip): 
-				   		if ($j < sizeof($loc['ips']) - 1) {
-				   	?>		<?= Html::encode($ip) ?>,
-					<?php } else {?>
-				   			<?= Html::encode($ip) ?>
-				   	<?php } 
-				   		$j++;
-				   	?>
-				   	<?php endforeach; ?>
-			   	</li>
-		   	</ul>
-		   <?php $i++ ?>
-		<?php endforeach; ?>
-	</ol>
+	<div style="float:left">
+		<h3><u>Alphabetical Listing of Logged IP addresses</u></h3>
+
+		<ol>
+			<?php $i = 0 ?>
+			<?php foreach ($locs as $loc): ?>
+			    <li><label> &nbsp IP location</label>: <?= Html::encode($loc['loc']) ?> - (count <?= Html::encode($loc['cnt']) ?>) &nbsp<span class="location-lbl glyphicon glyphicon-chevron-down" aria-hidden="true"> </span><span class="location-lbl glyphicon glyphicon-chevron-up" aria-hidden="true"> </span></li>
+		   		<ul class='bottom'>
+				   	<li><label>Status codes</label>:
+					   	<?php $j = 0 ?>
+					   	<?php foreach ($loc['status'] as $statusCode): 
+					   		if ($j < sizeof($loc['status']) - 1) {
+					   	?>		<?= Html::encode($statusCode) ?>,
+						<?php } else {?>
+					   			<?= Html::encode($statusCode) ?>
+					   	<?php } 
+					   		$j++;
+					   	?>
+					   	<?php endforeach; ?>
+				   	</li>
+				   	<li><label>IP Addresses</label>:
+					   	<?php $j = 0 ?>
+					   	<?php foreach ($loc['ips'] as $ip): 
+					   		if ($j < sizeof($loc['ips']) - 1) {
+					   	?>		<?= Html::encode($ip) ?>,
+						<?php } else {?>
+					   			<?= Html::encode($ip) ?>
+					   	<?php } 
+					   		$j++;
+					   	?>
+					   	<?php endforeach; ?>
+				   	</li>
+			   	</ul>
+			   <?php $i++ ?>
+			<?php endforeach; ?>
+		</ol>
+	</div>
+
+	<div  style="float:right">
+	    <?php
+	    	use scotthuangzl\googlechart\GoogleChart;
+
+	    	$myData = [];
+
+	    	$obj = ["location", "count"];
+	        array_push($myData, $obj);
+
+	    	foreach ($locs as $loc):
+	    		$obj = [$loc['loc'], $loc['cnt']];
+	            array_push($myData, $obj);
+	    	endforeach;
+
+	    	echo GoogleChart::widget(array('visualization' => 'PieChart',
+		        'data' => $myData,
+		        'options' => array('title' => 'Hits per Location')));
+	    ?>
+	</div>
 </div>
+
